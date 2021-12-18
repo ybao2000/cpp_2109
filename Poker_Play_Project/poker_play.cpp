8a3f6c316241b397691426f12b9df263a752a0ca
ybao2000 <ybao2000@gmail.com>
2021-12-18 14:59:48 -0700

    Lesson 14

--- Poker_Play_Project/poker_play.cpp
+++ Poker_Play_Project/poker_play.cpp
@@ -1,5 +1,11 @@
-#include <bits/stdc++.h> // this is only for g++
-// #include <iostream>
+// #include <bits/stdc++.h> // this is only for g++
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <random>
+#include <ctime>
+
 #include <io.h>
 #include <fcntl.h>
 using namespace std;
@@ -72,10 +78,16 @@ class Card {
   }
 };
 
+bool compare_pair(iPair p1, iPair p2) {
+  if(p1.first < p2.first) return false;
+  else if(p1.first > p2.first) return true;
+  else return p1.second > p2.second;
+}
+
 class Hand {
   private:
     wstring _player;
-    vector<Card> _cards;  // it contains all 5 cards
+    vector<Card> _cards;  // it contains all 5 cards in descending order
     Rank _rank;
     vector<iPair> _groups;
 
@@ -121,7 +133,7 @@ class Hand {
     for(auto item : mp){
       _groups.push_back({item.second, item.first});
     }
-    sort(_groups.begin(), _groups.end()); // pair wil be automatically sorted according to the first, then to the second
+    sort(_groups.begin(), _groups.end(), compare_pair); // pair wil be automatically sorted according to the first, then to the second
   }
 
   bool isFlush() {
@@ -133,7 +145,29 @@ class Hand {
   }
 
   bool isStraight() {
+    // check the _cards (in descending order)
+    int num = _cards[0].getNumber()+1;  // initial
+    for(auto card: _cards){
+      if(card.getNumber() != num-1) {
+        return false;
+      }
+      else {
+        num--;
+      }
+    }
+    return true;
+  }
 
+  bool isThreeOfAKind() {
+    return _groups[0].first == 3;
+  }
+
+  bool isTwoPairs() {
+    return _groups[0].first == 2 && _groups[1].first == 2;
+  }
+
+  bool isOnePair() {
+    return _groups[0].first == 2;
   }
 
   bool isFullHouse() {
@@ -147,7 +181,7 @@ class Hand {
   }
 
   bool isRoyalFlush() {
-    return isStraightFlush() && _groups[0].second == 14;
+    return isStraightFlush() && _cards[0].getNumber() == 14;
   }
   public:
   // constructor
@@ -158,27 +192,72 @@ class Hand {
   void setRank() {
     makeGroups();
     if(isRoyalFlush()) _rank = RoyalFlush;
-    else if(isStraight()) _rank = StraightFlush;
+    else if(isStraightFlush()) _rank = StraightFlush;
     else if(isFourOfAKind()) _rank = FourOfAKind;
     else if(isFullHouse()) _rank = FullHouse;
     else if(isFlush()) _rank = Flush;
+    else if(isStraight()) _rank = Straight;
+    else if(isThreeOfAKind()) _rank = ThreeOfAKind;
+    else if(isTwoPairs()) _rank = TwoPairs;
+    else if(isOnePair()) _rank = OnePair;
+    else _rank = HighHand;
   }
+
   void show() {
     wcout << _player << " : " << strCards() << " , " << strRank() << endl;
   }
 };
 
+vector<Card> deal() {
+  // this is going to generate 52 cards and shuffle them
+  vector<Card> cards;
+  for(int i=1; i<=4; i++) { // 4 suits
+    for(int j=2; j<=14; j++){ // 13 numbers
+      Suit suit = static_cast<Suit>(i);
+      cards.push_back(Card(suit, j));
+    }
+  }
+  srand(time(NULL));
+  // random_shuffle(cards.begin(), cards.end());
+  vector<Card> shuffled;
+  while (cards.size() > 0){
+    int num = cards.size();
+    int seqNo = rand()%num;
+    Card card = cards[seqNo];
+    shuffled.push_back(card);
+    cards.erase(cards.begin() + seqNo);
+  }
+  return shuffled;
+}
+
+bool compare_card(Card card1, Card card2) {
+  return card1.getNumber() > card2.getNumber();
+}
+
 int main() {
   _setmode(_fileno(stdout), _O_U16TEXT);  
+  vector<Card> shuffled = deal(); 
+  vector<Hand> hands;
+  auto it_begin = shuffled.begin();
+  for(int i=0; i<4; i++){
+    auto it_end = it_begin + 5;
+    vector<Card> cards(it_begin, it_end);
+    sort(cards.begin(), cards.end(), compare_card);
+    Hand hand(L"Player " + to_wstring(i+1), cards);
+    hand.setRank();
+    hand.show();
+    hands.push_back(hand);
+    it_begin = it_end;
+  }
   // Card c1 = Card(Heart, 12);
   // wcout << c1.to_string();
-  vector<Card> cards;
-  cards.push_back(Card(Heart, 12));
-  cards.push_back(Card(Heart, 12));
-  cards.push_back(Card(Heart, 12));
-  cards.push_back(Card(Heart, 12));
-  cards.push_back(Card(Heart, 12));   
-  Hand h = Hand(L"Player 1", cards);  
-  h.setRank(); 
-  h.show();  
+  // vector<Card> cards;
+  // cards.push_back(Card(Heart, 14));
+  // cards.push_back(Card(Club, 12));
+  // cards.push_back(Card(Heart, 11));
+  // cards.push_back(Card(Heart, 9));   
+  // cards.push_back(Card(Heart, 8));
+  // Hand h = Hand(L"Player 1", cards);  
+  // h.setRank(); 
+  // h.show();  
 }
